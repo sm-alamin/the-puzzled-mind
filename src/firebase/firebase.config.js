3fb3e366c8844b7ccce478a162b5ea0070030a5b
sm-alamin <sma.fkml@gmail.com>
2023-05-20 22:54:35 +0600

    environment variable is added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-ImprMkBVJT6J7tDUo-cL9Zb738qISQA",
-  authDomain: "the-puzzled-mind.firebaseapp.com",
-  projectId: "the-puzzled-mind",
-  storageBucket: "the-puzzled-mind.appspot.com",
-  messagingSenderId: "331755718949",
-  appId: "1:331755718949:web:f910b155db1afbde4ef2cc"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
